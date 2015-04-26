65b9f2daf539b11f0bcc5bd11283390b5ebcbb02
derwolfe <chriswwolfe@gmail.com>
2015-04-26 11:10:48 -0500

    play around

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject scratch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "mit clojure herumspielen"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
